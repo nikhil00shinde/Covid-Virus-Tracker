35b85597816b3033fb38dd43d93b567a774cffcc
Nikhil Shinde <nikhilshinde00@outlook.com>
2022-02-14 16:41:24 +0530

    complete project

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchDailyData } from "../../api/index";
+import { Line, Bar } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
 
-const Chart = () => {
-	return <h1>Chart</h1>;
+import styles from "./Chart.module.css";
+
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
+	const [dailyData, setDailyData] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			let data = await fetchDailyData();
+			setDailyData(data);
+		})();
+	}, []);
+
+	const lineChart = dailyData.length ? (
+		<Line
+			data={{
+				labels: dailyData.map(({ date }) => date),
+				datasets: [
+					{
+						data: dailyData.map(({ confirmed }) => confirmed),
+						label: "Infected",
+						borderColor: "#3333ff",
+						fill: true,
+					},
+					{
+						data: dailyData.map(({ deaths }) => deaths),
+						label: "Deaths",
+						borderColor: "red",
+						backgroundColor: "rgba(255,0,0,0.5)",
+						fill: true,
+					},
+				],
+			}}
+		/>
+	) : null;
+
+	const barChart = confirmed ? (
+		<Bar
+			data={{
+				labels: ["Infected", "Recovered", "Deaths"],
+				datasets: [
+					{
+						label: "People",
+						data: [confirmed.value, recovered.value, deaths.value],
+						backgroundColor: [
+							"rgba(0, 0, 255, 0.5)",
+							"rgba(0, 255, 0, 0.5)",
+							"rgba(255, 0, 0, 0.5)",
+						],
+					},
+				],
+			}}
+			options={{
+				plugins: {
+					legend: { display: false },
+					title: {
+						display: true,
+						text: `Current state in a ${country}`,
+					},
+				},
+			}}
+		/>
+	) : null;
+
+	return (
+		<div className={styles.container}>{country ? barChart : lineChart}</div>
+	);
 };
 
 export default Chart;
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FormControl, NativeSelect } from "@mui/material";
 
-const CountryPicker = () => {
-	return <h1>CountryPicker</h1>;
+import styles from "./CountryPicker.module.css";
+
+import { fetchCountries } from "../../api";
+
+const CountryPicker = ({ handleCountryChange }) => {
+	const [fetchedCountries, setFetchCountries] = useState([]);
+
+	useEffect(() => {
+		const fetchAPI = async () => {
+			setFetchCountries(await fetchCountries());
+		};
+
+		fetchAPI();
+	}, []);
+
+	return (
+		<FormControl className={styles.formControl}>
+			<NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+				<option value="">Global</option>
+				{fetchedCountries.map((country, i) => (
+					<option key={i} value={country}>
+						{country}
+					</option>
+				))}
+			</NativeSelect>
+		</FormControl>
+	);
 };
 
 export default CountryPicker;
